acca816c2ef76b0b342dd7f9cec78a63979f8c2a
RQuicho <raymondquicho@gmail.com>
2023-05-25 23:38:04 -0700

    Updating models.py and seed.py to include tag class

--- models.py
+++ models.py
@@ -30,8 +30,29 @@ class Post(db.Model):
 	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 	user = db.relationship('User', backref='posts')
+	hashtags = db.relationship('PostTag', backref='post')
+	tags = db.relationship('Tag', secondary='posts_tags', backref='posts')
 
 	def __repr__(self):
 		return f"< Post {self.title} {self.content} {self.created_at} {self.user_id} >"
 
+class PostTag(db.Model):
+	__tablename__ = 'posts_tags'
+
+	post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+	tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+
+	def __repr__(self):
+		return f"< PostTag {self.post_id} {self.tag_id} >"
+
+class Tag(db.Model):
+	__tablename__ = 'tags'
+
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name = db.Column(db.Text, unique=True)
+
+	hashtags = db.relationship('PostTag', backref='tag')
+
+	def __repr__(self):
+		return f"< Tag {self.name} >"
 
--- seed.py
+++ seed.py
@@ -1,6 +1,6 @@
 """Seed file to make sample data for blogly db"""
 
-from models import db, User, Post
+from models import db, User, Post, PostTag, Tag
 from app import app
 
 # Create all tables
@@ -9,21 +9,40 @@ db.create_all()
 
 # If table isn't empty, empty it
 User.query.delete()
+Post.query.delete()
+PostTag.query.delete()
+Tag.query.delete()
+
+# ******* USERS **************
 
 # Add users
-Alan = User(first_name='Alan', last_name='Alda', image_url='https://i0.web.de/image/938/34260938,pd=1/alan-walker.jpg')
-Joel = User(first_name='Joel', last_name='Burton', image_url='https://wtop.com/wp-content/uploads/2017/01/joel-mchale2.jpg')
-Jane = User(first_name='Jane', last_name='Smith', image_url='https://i.pinimg.com/originals/37/e4/82/37e4825fb2e5d4f775746752e0ae79d2.jpg')
-
-# Add posts
-Recepie = Post(title='Chicken Katsu', content='This is how you make chicken katsu.', user_id=1)
-Survival = Post(title='Navigation', content='I will teach you how to navigate the wilderness if you get lost.', user_id=2)
-Gorillas = Post(title='Observation 4', content='I may have seen a wild man amongst the troop.', user_id=3)
+tess = User(first_name='Tess', last_name='Servopoulos', image_url='https://news-cdn.softpedia.com/images/news2/The-Last-of-Us-Introduces-Tess-Joel-s-Partner-2.jpg')
+joel = User(first_name='Joel', last_name='Miller', image_url='https://wallpapercave.com/wp/wp6849598.jpg')
+ellie = User(first_name='Ellie', last_name='Williams', image_url='https://img5.goodfon.com/wallpaper/nbig/c/13/the-last-of-us-part-ii-ellie-games-ps4-naughty-dog.jpg')
 
 # Add new users to session
-db.session.add_all([Alan, Joel, Jane])
+db.session.add_all([tess, joel, ellie])
 db.session.commit()
 
+# ******* POSTS **************
+
+# Add posts
+feelings = Post(title='Not Sure', content="I'm not sure if I can act on these feelings.", user=tess)
+navigation = Post(title='Navigation', content='I will teach you how to navigate the wilderness if you get lost.', user=joel)
+my_girl = Post(title='My Girl', content='She reminds me of her so much.', user=joel)
+trust = Post(title='Trust No One', content='Keep it a secret and you will live.', user=ellie)
+
 # Add new posts to session
-db.session.add_all([Recepie, Survival, Gorillas])
+db.session.add_all([feelings, navigation, my_girl, trust])
+db.session.commit()
+
+
+# ******* TAGS **************
+
+# Add tags
+t1 = Tag(name='love', hashtags=[PostTag(post_id=feelings.id)])
+t2 = Tag(name='survive', hashtags=[PostTag(post_id=trust.id)])
+
+# Add new tags to session
+db.session.add_all([t1, t2])
 db.session.commit()
